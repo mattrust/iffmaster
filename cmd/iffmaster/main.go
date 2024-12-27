339079e82b5081c6d0d6fee5eb9403bb2c3365a2
Matthias Rustler <mrustler@gmx.de>
2024-12-27 12:22:14 +0100

    update usage instructions to include optional filename parameter for IFF file inspection

--- cmd/iffmaster/main.go
+++ cmd/iffmaster/main.go
@@ -4,11 +4,13 @@
 /*
 Iffmaster is a tool to inspect IFF files as defined under EA-85.
 
-Usage: [options]
+Usage: [options] filename
 
 	iffmaster
 
 		-version: Show the application's version.
+
+		filename: The IFF file to inspect (optional).
 */
 package main
 
@@ -24,6 +26,13 @@ func main() {
 	var filename string
 
 	showVersion := flag.Bool("version", false, "Display the version of iffmaster")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"Usage: %s [options] filename\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"\n  filename: The IFF file to inspect (optional).\n")
+	}
 	flag.Parse()
 
 	if *showVersion {
